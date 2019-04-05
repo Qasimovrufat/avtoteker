720d8a5411b6c64a1550c2882089621902597ac5
Shohrat M. Maharramov <shohrat.maharramov@bakubus.local>
2019-04-05 18:01:16 +0400

    search bitdi

--- js/script.js
+++ js/script.js
@@ -13,4 +13,22 @@ $(document).ready(function(){
    
     $('select').selectpicker();
 
+    $("#ex2").slider({});
+    $("#ex1").slider({});
+
+    // Without JQuery
+   $(".Reset").click(function(params) {
+      
+       $(this).parents("form").reset()
+   })
+
+   $(".tab-list a").click(function(e) {
+       console.log("sdsad")
+       e.preventDefault()
+       $(".tab-list a").removeClass("active");
+       $(this).addClass("active");
+       $(".tab-content").removeClass("active")
+       var activetab=$(this).attr("href")
+       $(activetab).addClass("active")
+   })
 })
